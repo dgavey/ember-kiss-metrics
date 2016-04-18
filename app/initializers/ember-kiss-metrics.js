e92437d3263fcdd69a0df0ed5085d0af2d5d9f52
Derek Gavey <dgavey@gavey.ca>
2016-04-18 10:28:11 -0600

    Make container injection 1 and 2 compatible

--- app/initializers/ember-kiss-metrics.js
+++ app/initializers/ember-kiss-metrics.js
@@ -1,4 +1,10 @@
-export function initialize(application) {
+export function initialize() {
+  let application = arguments[1] || arguments[0];
+  if (arguments[1]) {
+    //for ember 1.x
+    const container = arguments[0];
+    container.options('kmq:main'); 
+  }
   application.inject('controller', '_kmq', 'kmq:main');
   application.inject('route',      '_kmq', 'kmq:main');
 };
