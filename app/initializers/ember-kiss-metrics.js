678559ce0c6d951e654d7c61ba5e67a41c0d20dc
Derek Gavey <dgavey@gavey.ca>
2016-05-09 12:45:29 -0600

    Tweak to injection code for clarity

--- app/initializers/ember-kiss-metrics.js
+++ app/initializers/ember-kiss-metrics.js
@@ -1,8 +1,9 @@
 export function initialize() {
-  let application = arguments[1] || arguments[0];
-  if (arguments[1]) {
+  let application = arguments[0];
+  if (arguments.length === 2) {
     //for ember 1.x
     const container = arguments[0];
+    application =  arguments[1];
     container.options('kmq:main'); 
   }
   application.inject('controller', '_kmq', 'kmq:main');
