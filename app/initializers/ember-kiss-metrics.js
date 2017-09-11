c1e2bd3019f2db27d14dfe6cd50ae35ecc6a958f
Derek Gavey <dgavey@gavey.ca>
2017-09-11 12:01:35 -0600

    Makes addon compatible with fastboot

--- app/initializers/ember-kiss-metrics.js
+++ app/initializers/ember-kiss-metrics.js
@@ -1,13 +1,16 @@
 export function initialize() {
-  let application = arguments[0];
-  if (arguments.length === 2) {
-    //for ember 1.x
-    const container = arguments[0];
-    application =  arguments[1];
-    container.options('kmq:main'); 
+  if (typeof FastBoot === 'undefined') {
+    // only execute this in browser
+    let application = arguments[0];
+    if (arguments.length === 2) {
+      //for ember 1.x
+      const container = arguments[0];
+      application =  arguments[1];
+      container.options('kmq:main');
+    }
+    application.inject('controller', '_kmq', 'kmq:main');
+    application.inject('route',      '_kmq', 'kmq:main');
   }
-  application.inject('controller', '_kmq', 'kmq:main');
-  application.inject('route',      '_kmq', 'kmq:main');
 };
 
 export default {
